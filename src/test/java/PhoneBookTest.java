2c4b1d3e8c046fad19dd7accdc22f11ed709bc05
Oleg Sichev <sichev.oleg@bk.ru>
2023-07-09 15:17:52 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -16,4 +16,20 @@ public class PhoneBookTest {
         Assert.assertEquals(-1, result);
     }
 
+    @Test
+    public void testFindByNumber(){
+        phoneBook.add("John", "12345");
+
+        String result = phoneBook.findByNumber("12345");
+        Assert.assertEquals("John", result);
+
+        phoneBook.add("Petya", "321");
+        result = phoneBook.findByNumber("321");
+        Assert.assertEquals("Petya", result);
+
+        result = phoneBook.findByNumber("1111");
+        Assert.assertNull(result);
+
+    }
+
 }
